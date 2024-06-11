dc499ce57cfdd3407ff221cab246594aafbef82b
brogramer <106384917+user0101100@users.noreply.github.com>
2024-06-11 02:12:14 -0700

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -4,8 +4,10 @@ class Carousel {
         this.carouselImages = container.querySelector('.carousel-images');
         this.imageContainers = container.querySelectorAll('.carousel-images a');
         this.images = container.querySelectorAll('.carousel-images img[data-src]');
+        this.thumbnails = container.querySelectorAll('.carousel-thumbnails img');
         this.prevButton = container.querySelector('.prev');
         this.nextButton = container.querySelector('.next');
+        this.fullscreenButton = container.querySelector('.fullscreen');
         this.indicators = container.querySelector('.carousel-indicators');
         this.index = 0;
         this.totalImages = this.images.length;
@@ -14,6 +16,8 @@ class Carousel {
         this.transitionDuration = options.transitionDuration || 0.5;
         this.autoPlay = options.autoPlay !== false;
         this.pauseOnHover = options.pauseOnHover !== false;
+        this.fullscreenEnabled = options.fullscreen !== false;
+        this.captions = options.captions || [];
 
         this.updateCarousel = this.updateCarousel.bind(this);
         this.next = this.next.bind(this);
@@ -22,11 +26,16 @@ class Carousel {
         this.stopAutoSlide = this.stopAutoSlide.bind(this);
         this.startAutoSlide = this.startAutoSlide.bind(this);
         this.lazyLoadImages = this.lazyLoadImages.bind(this);
+        this.toggleFullScreen = this.toggleFullScreen.bind(this);
+        this.updateCaption = this.updateCaption.bind(this);
 
         this.carouselImages.style.transition = `transform ${this.transitionDuration}s ${this.transitionEffect}`;
 
         this.prevButton.addEventListener('click', this.prev);
         this.nextButton.addEventListener('click', this.next);
+        if (this.fullscreenEnabled && this.fullscreenButton) {
+            this.fullscreenButton.addEventListener('click', this.toggleFullScreen);
+        }
 
         if (this.pauseOnHover) {
             this.container.addEventListener('mouseenter', this.stopAutoSlide);
@@ -37,12 +46,20 @@ class Carousel {
         this.carouselImages.addEventListener('touchmove', this.touchMove.bind(this));
         this.carouselImages.addEventListener('touchend', this.touchEnd.bind(this));
 
+        this.thumbnails.forEach((thumbnail, idx) => {
+            thumbnail.addEventListener('click', () => {
+                this.index = idx;
+                this.updateCarousel();
+            });
+        });
+
         if (this.autoPlay) {
             this.startAutoSlide();
         }
 
         this.createIndicators();
         this.lazyLoadImages(); // Initial lazy load
+        this.updateCaption(); // Initial caption update
     }
 
     createIndicators() {
@@ -68,6 +85,7 @@ class Carousel {
         this.carouselImages.style.transform = `translateX(${-this.index * 100}%)`;
         this.updateIndicators();
         this.lazyLoadImages(); // Lazy load on carousel update
+        this.updateCaption(); // Update caption on carousel update
     }
 
     next() {
@@ -128,6 +146,23 @@ class Carousel {
                 image.removeAttribute('data-src');
             }
         });
+    }
+
+    toggleFullScreen() {
+        if (!document.fullscreenElement) {
+            this.container.requestFullscreen().catch(err => {
+                alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+            });
+        } else {
+            document.exitFullscreen();
+        }
+    }
+
+    updateCaption() {
+        const captionContainer = this.container.querySelector('.carousel-caption');
+        if (captionContainer) {
+            captionContainer.textContent = this.captions[this.index] || '';
+        }
     }
 }
 
